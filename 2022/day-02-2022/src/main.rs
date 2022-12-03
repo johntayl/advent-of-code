cc2c1e75e77183d9cf0c35d2f852039d1d9c7b31
John Taylor <johntayl89@gmail.com>
2022-12-03 01:11:09 -0500

    feat(day3): 2022 Day 3 solution

--- 2022/day-02-2022/src/main.rs
+++ 2022/day-02-2022/src/main.rs
@@ -4,22 +4,23 @@ fn main() {
 
     // A, B, C = rock, paper, scissors
     // X, Y, Z = rock, paper, scissors
-
     let input = include_str!("input.txt");
+    let games = input.lines().map(|line| {
+        return line.split_whitespace().collect::<Vec<&str>>()
+    }).collect::<Vec<Vec<&str>>>();
         
-    let mut total_score=  0;    
+    let mut total_score_1=  0;    
+    let mut total_score_2 = 0;
 
-    for line in input.lines() {
-        let game = line.split_whitespace().collect::<Vec<&str>>();
-        
-        // let score = score(&game[0], &game[1]);
-        let score = strategy(&game[0], &game[1]);
-        
-        total_score += score;
+    for game in games {
+        total_score_1 += score(&game[0], &game[1]);
+        total_score_2 += strategy(&game[0], &game[1]);
     }
 
     // part 1
-    println!("input: {:?}", total_score);
+    println!("Score 1: {:?}", total_score_1);
+    // part 2
+    println!("Score 2: {:?}", total_score_2);
 }
 
 fn score(a: &str, b: &str) -> usize {
@@ -77,7 +78,6 @@ fn score(a: &str, b: &str) -> usize {
 
     return 0;
 }
-
 
 fn strategy(a: &str, b: &str) -> usize {
     // X = lose
---
+++ 2022/day-03-2022/src/main.rs
@@ -0,0 +1,91 @@
+use std::{collections::HashMap, ops::Index};
+
+fn split_string_half(string: &str) -> (String, String) {
+    let half = string.len() / 2;
+    let (first, second) = string.split_at(half);
+    return (first.to_string(), second.to_string())
+}
+
+fn get_priority(letters: &str, letter: char) -> usize {
+    let mut priority = 1;
+    for l in letters.chars() {
+        if l == letter {
+            return priority;
+        }
+        priority += 1;
+    }
+    return priority;
+}
+
+fn find_match_char(first: &str, second: &str) -> Option<char> {
+    for c in first.chars() {
+        if second.contains(c) {
+            return Some(c);
+        }
+    }
+    return None;
+}
+
+
+fn main() {
+    let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let input = include_str!("input.txt");
+
+    let mut total_score = 0;
+    let mut total_score_2 = 0;
+
+    // part 2
+    // every 3 lines is a group
+    let mut group_chars: HashMap<char, Vec<i32>> = HashMap::new();
+    let mut current_group_count = 0;
+
+    // part 1
+    for line in input.lines() {
+        let (first, second) = split_string_half(line);
+        let matching_char = find_match_char(&first, &second);
+        if let Some(match_char) =  matching_char {
+            let priority = get_priority(letters, match_char);
+            total_score += priority;
+        }
+
+        // part 2
+        if current_group_count == 3  {
+            // on to final line, do priority calc and clear
+            for (key, value) in &group_chars {
+                if value.len() == 3 { 
+                    total_score_2 += get_priority(letters, *key);
+                }
+            }
+            group_chars.clear();
+            current_group_count = 0;
+        }
+
+        current_group_count += 1;
+
+        for c in line.chars() {
+            // count once per line
+            if !group_chars.contains_key(&c) {
+                group_chars.insert(c, vec![current_group_count]);
+            } else {
+                // push current group if not already in
+                let group_vec = group_chars.get_mut(&c).unwrap();
+                if !group_vec.contains(&current_group_count) {
+                    group_vec.push(current_group_count);
+                }                
+            }
+        }
+    }
+
+    // final group, need to get priority one last time.
+    for (key, value) in &group_chars {
+        if value.len() == 3 { // .
+            total_score_2 += get_priority(letters, *key);
+        }
+    }
+
+    // part 1
+    println!("Score: {}", total_score);
+
+    // part 2
+    println!("Score 2: {}", total_score_2);
+}
